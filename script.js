4fbf1aa4afcf6f49c456006f7bad3a97d3d363d1
Romanov-E <72961904+Romanov-E@users.noreply.github.com>
2024-06-10 15:43:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById('hours')
 const minsEl = document.getElementById('mins')
 const secondsEl = document.getElementById('seconds')
 
-const newYears = '6 jul 2024'
+const newYears = '6 jul 2024, 04:00:00'
 
 function countdown() {
     const newYearsDate = new Date(newYears);
